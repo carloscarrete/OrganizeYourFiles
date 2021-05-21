3fd9bbef6d4008ca292e2c684012bb79e3b454aa
Carlos Carrete <carretecarlosg@gmail.com>
2021-05-21 01:41:06 -0500

    Se añadio una comprobacion para dir y file ademas de otros ciclos para ordenar mas extensiones

--- Order.py
+++ Order.py
@@ -3,34 +3,48 @@ import shutil
 
 downloads_path = '/home/carlos/Downloads/'
 images = ('.jpg','.png','.jpeg','.gif')
-video = ('.wmv','.avi','.mp4','.wmv','.mov')
+video = ('.wmv','.avi','.mp4','.wmv','.mov','.mkv')
 text = ('.doc','.docx','.pdf','.txt')
-"""
-if not os.path.exists('/home/carlos/Desktop/Archivos'):
-    os.mkdir('/home/carlos/Desktop/Archivos')
-else:
-    print('Ya existe')
-name = "Nota.txt"
-shutil.move(downloads_path+'/Nota.txt','/home/carlos/Desktop/Archivos/'+name)
-"""
+compressed = ('.rar','.zip')
 
 def organize(file,ext):
     for order in images:
+        folderCreation('Images')
         if ext == order:
-            shutil.move(downloads_path+file,'/home/carlos/Desktop/'+file)
+            shutil.move(downloads_path+file,downloads_path+'/Images/'+file)
+    for order in video:
+        folderCreation('Videos')
+        if ext == order:
+            shutil.move(downloads_path+file,downloads_path+'/Videos/'+file)
+    for order in compressed:
+        folderCreation('Compressed')
+        if ext == order:
+            shutil.move(downloads_path+file,downloads_path+'/Compressed/'+file)
+    for order in text:
+        folderCreation('TextFiles')
+        if ext == order:
+            shutil.move(downloads_path+file,downloads_path+'/TextFiles/'+file)
+
+    if ext =="":
+        if not os.path.isdir(downloads_path+file):
+            folderCreation('Others')
+            shutil.move(downloads_path+file,downloads_path+'/Others/'+file)
 
 
 def folderCreation(FolderName):
     folderName = FolderName
-    if not os.path.exists(folderName):
+    if not os.path.exists(downloads_path+'/'+folderName):
         os.mkdir(downloads_path+'/'+folderName)
         print(folderName + ' has been created!')
 
-for fileX in os.listdir(downloads_path):
-    name, ext = os.path.splitext(fileX)
-    organize(fileX,ext)
-'''
-print(os.listdir(downloads_path))
-print('-----------------')
-print(os.path.splitext(downloads_path))
-'''
+
+def main():
+    for fileX in os.listdir(downloads_path):
+        name, ext = os.path.splitext(fileX)
+        organize(fileX,ext)
+    print('-*-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-*-*')
+    print('All files were organized correctly.!')
+
+
+if __name__=='__main__':
+    main()
