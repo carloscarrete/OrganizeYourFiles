00ca6f1d4183a9b07efc7ac793a134d93b50c925
Carlos Carrete <carretecarlosg@gmail.com>
2021-05-21 00:32:23 -0500

    Se añadio el primer metodo para organizar los archivos de imagenes

--- Order.py
+++ Order.py
@@ -1,14 +1,31 @@
 import os
 import shutil
 
-downloads_path = '/home/carlos/Downloads'
+downloads_path = '/home/carlos/Downloads/'
 images = ('.jpg','.png','.jpeg','.gif')
 video = ('.wmv','.avi','.mp4','.wmv','.mov')
 text = ('.doc','.docx','.pdf','.txt')
-
+"""
 if not os.path.exists('/home/carlos/Desktop/Archivos'):
     os.mkdir('/home/carlos/Desktop/Archivos')
 else:
     print('Ya existe')
 name = "Nota.txt"
 shutil.move(downloads_path+'/Nota.txt','/home/carlos/Desktop/Archivos/'+name)
+"""
+
+def organize(file,ext):
+    for order in images:
+        if ext == order:
+            shutil.move(downloads_path+file,'/home/carlos/Desktop/'+file)
+
+
+
+for fileX in os.listdir(downloads_path):
+    name, ext = os.path.splitext(fileX)
+    organize(fileX,ext)
+'''
+print(os.listdir(downloads_path))
+print('-----------------')
+print(os.path.splitext(downloads_path))
+'''
