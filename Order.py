f52177810cacc149aeb389cb39c15ff513bf28cf
Carlos Carrete <carretecarlosg@gmail.com>
2021-05-25 20:31:40 -0500

    Optmizacion de codigo, lista de tuplas

--- Order.py
+++ Order.py
@@ -1,30 +1,35 @@
-import os, shutil, sys, getopt
+import os, shutil, sys, getopt, itertools
 
 downloads_path = None
+'''
 images = ('.jpg','.png','.jpeg','.gif')
 video = ('.wmv','.avi','.mp4','.wmv','.mov','.mkv')
 text = ('.doc','.docx','.pdf','.txt')
 compressed = ('.rar','.zip')
 dot = images + video + text + compressed
-def organize(file,ext):
-    if ext in (dot):
-        for order in images:
-            folderCreation('Images')
-            if ext == order:
-                shutil.move(downloads_path+file,downloads_path+'/Images/'+file)
-        for order in video:
-            folderCreation('Videos')
-            if ext == order:
-                shutil.move(downloads_path+file,downloads_path+'/Videos/'+file)
-        for order in compressed:
-            folderCreation('Compressed')
-            if ext == order:
-                shutil.move(downloads_path+file,downloads_path+'/Compressed/'+file)
-        for order in text:
-            folderCreation('TextFiles')
-            if ext == order:
-                shutil.move(downloads_path+file,downloads_path+'/TextFiles/'+file)
+'''
+extensions = [('.jpg','.png','.jpeg','.gif'),
+        ('.wmv','.avi','.mp4','.wmv','.mov','.mkv'),
+        ('.doc','.docx','.pdf','.txt'),
+        ('.rar','.zip')]
+extComp = [item for ext in extensions for item in ext]
 
+def organize(file,ext):
+    if ext in extComp:
+        for order in extComp:
+            folderCreation('Images')
+            folderCreation('Videos')
+            folderCreation('Compressed')
+            folderCreation('TextFiles')
+            if ext in extensions[0]:
+                shutil.move(downloads_path+file,downloads_path+'/Images/'+file)
+            if ext in extensions[1]:
+                shutil.move(downloads_path+file,downloads_path+'/Videos/'+file)
+            if ext in extensions[2]:
+                shutil.move(downloads_path+file,downloads_path+'/Compressed/'+file)
+            if ext in extensions[3]:
+                shutil.move(downloads_path+file,downloads_path+'/TextFiles/'+file)
+            return
     else:
         if os.path.isfile(downloads_path+file) and not os.path.isdir(downloads_path+file):
             folderCreation('Others')
@@ -56,8 +61,6 @@ def checkArgvs():
         if op in ('-h','--help'):
             print('Help')
             
-
-
 def main():
     checkArgvs()
     if downloads_path != None:
@@ -68,6 +71,7 @@ def main():
             #print(downloads_path+'/'+fileX +' has been moved correctly!')
         print('-*-*-*-*-*-*-*-*-*-**-*-*-*-*-*-*-*-*')
         print('All files were organized correctly.!')
+        print('New Version')
 
 
 if __name__=='__main__':
